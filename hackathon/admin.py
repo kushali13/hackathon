796f22638dfe0af33c85e120619875a16c6d340d
Virendrasingh <virendrasinghpawar13@gmail.com>
2025-03-30 23:20:33 +0530

    Add CustomUser model and enhance Course model with additional fields

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course , CustomUser
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title',)  # Display course title in admin panel
+    list_display = ('title', 'instructor')  # Display course title in admin panel
+
+admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'user_type') 
+    
--- hackathon/models.py
+++ hackathon/models.py
@@ -6,9 +6,26 @@ class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='media/courses/')
+    instructor = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='courses')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+    is_enrolled = models.BooleanField(default=False)
+    enrollment_start_date = models.DateField()
+    enrollment_end_date = models.DateField()
+    enrollment_limit = models.PositiveIntegerField()
+    enrolled_students = models.PositiveIntegerField(default=0)
+    syllabus = models.TextField()
+    prerequisites = models.TextField()
+    course_materials = models.TextField()
+    
 
     def __str__(self):
-        return self.title
+        return f"{self.title} taught by {self.instructor.username}"  
 
 class CustomUser(AbstractUser):
     USER_TYPE_CHOICES = (
